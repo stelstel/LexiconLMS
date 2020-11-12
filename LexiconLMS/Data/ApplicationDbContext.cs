8153929cd941bce57283f0cc13f9cd68da29aa1e
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-11-12 15:14:34 +0100

    Fix IdentityDbContext

--- LexiconLMS/Data/ApplicationDbContext.cs
+++ LexiconLMS/Data/ApplicationDbContext.cs
@@ -4,15 +4,18 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using LexiconLMS.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace LexiconLMS.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
         public DbSet<LexiconLMS.Models.Entities.AppUser> AppUser { get; set; }
+
+        // TODO Has query filter?
     }
 }
