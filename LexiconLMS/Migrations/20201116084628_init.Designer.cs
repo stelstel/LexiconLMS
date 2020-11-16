86a931ab858a3976b87ebeb125c32f13f60ee8e3
nokto <david.nokto@gmail.com>
2020-11-16 09:51:59 +0100

    Fixed small seed stuff

--- LexiconLMS/Migrations/20201116084628_init.Designer.cs
+++ LexiconLMS/Migrations/20201116084628_init.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LexiconLMS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20201113141057_init")]
+    [Migration("20201116084628_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
