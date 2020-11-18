32fc1aa71dfa32b0d1c9031201db83e2318ac659
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-11-18 16:55:39 +0100

    Allow Student to se user account list

--- LexiconLMS/Controllers/AppUsersController.cs
+++ LexiconLMS/Controllers/AppUsersController.cs
@@ -340,7 +340,7 @@ namespace LexiconLMS.Controllers
             return model;
         }
 
-        [Authorize(Roles = "Teacher")]
+        //[Authorize(Roles = "Teacher")]
         [Authorize(Roles = "Student")]
         public async Task<IActionResult> TeacherUserIndex()
         {
