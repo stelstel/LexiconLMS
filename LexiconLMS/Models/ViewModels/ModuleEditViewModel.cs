4f494ac8d37a92f7629f28afd439b76b94e3147a
nokto <david.nokto@gmail.com>
2020-12-01 15:11:50 +0100

    Changes in datetimes for Module Edit.

--- LexiconLMS/Models/ViewModels/ModuleEditViewModel.cs
+++ LexiconLMS/Models/ViewModels/ModuleEditViewModel.cs
@@ -17,12 +17,13 @@ namespace LexiconLMS.Models.ViewModels
         [DisplayName("Name")]
         public string ModuleName { get; set; }
         [DisplayName("Description")]
+        
         public string ModuleDescription { get; set; }
         [DisplayName("Start Time")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-ddTHH:mm}")]
         public DateTime ModuleStartTime { get; set; }
         [DisplayName("End Time")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-ddTHH:mm}")]
         public DateTime ModuleEndTime { get; set; }
 
 
@@ -35,10 +36,10 @@ namespace LexiconLMS.Models.ViewModels
         [DisplayName("Description")]
         public string ActivityDescription { get; set; }
         [DisplayName("Start Time")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-ddTHH:mm}")]
         public DateTime ActivityStartTime { get; set; }
         [DisplayName("End Time")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-ddTHH:mm}")]
         public DateTime ActivityEndTime { get; set; }
         [DisplayName("Type")]
         public int ActivityTypeId { get; set; }
